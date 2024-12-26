e58d92bdaf73b3832edb0b0032a8de6f296243f7
Sunny Yadav <sunny9662914546@gmail.com>
2024-12-25 23:55:01 -0500

    React Blog Application

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,45 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
 
-  const [blogs, setBlogs] = useState([
-    {title: 'Blog 1', body: 'Lorem ipsum dolor sit amet', author: 'Author 1', id: 1},
-    {title: 'Blog 2', body: 'Lorem ipsum dolor sit amet', author: 'Author 2', id: 2},
-    {title: 'Blog 3', body: 'Lorem ipsum dolor sit amet', author: 'Author 3', id: 3},
-  ]);
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState( null)
+
+  // Use effect is going to run everytime when something changes on the screen
+  // To make or only run the use effect certain times, add dependency to it by
+  // adding [] parameter after it.
+  // An empty [] means to run only a single time.
+  // If it should run after blogs change, add [blogs] to it.
+  useEffect(() => {
+    setTimeout(() =>
+    {
+      fetch("http://localhost:8000/blogs")
+      .then(res => {
+        if (!res.ok) {
+          throw Error("Could not fetch data");
+        }
+        return res.json()
+      })
+      .then(data => {
+        setBlogs(data);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch(err => {
+        setIsPending(false)
+        setError(err.message)
+      });
+    }, 1000);
+  }, [])
 
   return (
 
       <div className="home">
-        {blogs.map((blog) => (
-            <div className="blog-preview" key={blog.id}>
-              <h2>{blog.title}</h2>
-              <p>Written by: {blog.author}</p>
-            </div>
-        ))}
+        {error && <div>{error}</div>}
+        {isPending && <div>Loading...</div>}
+        {blogs && <BlogList blogsProp={blogs}></BlogList>}
       </div>
   );
 
